e5f46d080cdc0c42e44dea81b52503fe3070cb74
kzyz <yamazaki@gluum.com>
2025-02-19 13:45:54 +0900

    i2c_read_dt -> i2c_burst_read_dt

--- src/az1uball.c
+++ src/az1uball.c
@@ -78,7 +78,7 @@ void az1uball_read_data_work(struct k_work *work)
     int ret;
 
     // Read data from I2C
-    ret = i2c_read_dt(&config->i2c, buf, sizeof(buf));
+    ret = i2c_burst_read_dt(&config->i2c, REG_LEFT, buf, sizeof(buf));
     if (ret) {
         LOG_ERR("Failed to read movement data from AZ1YBALL: %d", ret);
         return;
